415f558efb681c4d90cd9d98ff717cc2b21185d5
Min Zhu <zhumin0315@foxmail.com>
2019-09-12 00:27:36 -0400

    update

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 from django.utils import timezone
-from django.urls import reverse
-# SuperUserInformation
-# User: Jose
-# Email: training@pieriandata.com
-# Password: testpassword
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
 class Post(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -24,12 +20,14 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post_detail",kwargs={'pk':self.pk})
 
+
     def __str__(self):
         return self.title
 
 
+
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
+    post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from blog.models import Post, Comment
 from django.utils import timezone
-from .models import Post, Comment
-from .forms import PostForm, CommentForm
+from blog.forms import PostForm, CommentForm
 
 from django.views.generic import (TemplateView,ListView,
                                   DetailView,CreateView,
@@ -11,10 +11,8 @@ from django.views.generic import (TemplateView,ListView,
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
 class AboutView(TemplateView):
-    template_name = 'blog/about.html'
-
+    template_name = 'about.html'
 
 class PostListView(ListView):
     model = Post
@@ -44,20 +42,19 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
     model = Post
 
 
-class PostDeleteView(LoginRequiredMixin,DeleteView):
-    model = Post
-    success_url = reverse_lazy('post_list')
-
-
 class DraftListView(LoginRequiredMixin,ListView):
     login_url = '/login/'
-    redirect_field_name = 'blog/post_draft_list.html'
+    redirect_field_name = 'blog/post_list.html'
 
     model = Post
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
 
 #######################################
 ## Functions that require a pk match ##
